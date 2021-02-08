c40d2e6cc670cbbd0e3844c45225875ad2725aa5
Nick Doiron <ndoiron@mapmeld.com>
2021-02-08 14:04:41 -0700

    comment form detail

--- src/components/comment-form.js
+++ src/components/comment-form.js
@@ -37,11 +37,28 @@ export default class CommentForm extends Component {
 
   render() {
     return <div>
-      <h3>Post a Comment</h3>
+      <h2>Post a Comment</h2>
       <p>
-        Submit a comment for review. If your comment passes quality standards,
-        it will be visible on our public comments site.
+        Welcome to the <a href="#">Ohio Citizens Redistricting Commission</a>
+        public input portal.
+        Tell the CRC what’s important to you! You can do so with written testimony,
+        by submitting a Community of Interest map, or by drawing your own districting plan.
       </p>
+      <p>
+        A Community of Interest (COI) is an area where residents are united by a common
+        interest that could be affected by legislation. Interests can be, for example,
+        social, cultural, economic, ethnic, environmental, or historical. By identifying
+        COIs, districters will know which areas are important to keep whole in a district.
+      </p>
+      <p>
+        Districting plans are maps of Congressional, State Legislative, or other districts.
+        Federal law requires that all districting plans have very close to equal population
+        per district and that they do not prevent minority groups from electing candidates of choice.
+      </p>
+      <i>
+        If your comment passes quality standards,
+        it will be visible on our public comments site.
+      </i>
       <i>This demo page currently accepts only links and text comments.
         File uploads will be added in the near future.
       </i>
@@ -60,16 +77,62 @@ export default class CommentForm extends Component {
         </div>
         <div className="form-group">
           <label className="form-label">Your Name</label>
-          <input
-            type="text"
-            name="name"
-            className="form-control"
-            autoComplete="off"
-            autofill="off"
-            onChange={e => this.setState({ postName: e.target.value })}
-            required
-            value={this.state.postName}/>
+          <div className="input-group">
+            <input
+              type="text"
+              name="first"
+              className="form-control"
+              autoComplete="off"
+              autofill="off"
+              onChange={e => this.setState({ postName: e.target.value })}
+              placeholder="First/Personal"
+              required
+              value={this.state.postName}/>
+            <input
+              type="text"
+              name="last"
+              className="form-control"
+              autoComplete="off"
+              autofill="off"
+              onChange={e => this.setState({ lastName: e.target.value })}
+              placeholder="Last/Family"
+              required
+              value={this.state.lastName}/>
+          </div>
         </div>
+        <div className="form-group">
+          <label className="form-label">Pronouns</label>
+          <input name="pronouns" type="text" className="form-control"
+            autoComplete="off" autofill="off"
+            />
+        </div>
+        <div className="form-group">
+          <label className="form-label">E-mail address</label>
+          <input name="email" type="email" className="form-control"
+            autoComplete="off" autofill="off"
+            />
+        </div>
+        <div className="form-group">
+          <label className="form-label">Residence</label>
+          <div className="input-group">
+            <input
+              type="text"
+              name="place"
+              className="form-control"
+              autoComplete="off"
+              autofill="off"
+              placeholder="Place"/>
+            <input
+              type="text"
+              name="state"
+              className="form-control"
+              autoComplete="off"
+              autofill="off"
+              placeholder="State"/>
+          </div>
+        </div>
+        <hr/>
+        <strong>What are you submitting?</strong>
         <div className="form-group">
           <label className="form-label">Link (optional)</label>
           <input
@@ -83,9 +146,36 @@ export default class CommentForm extends Component {
             value={this.state.postLink}/>
         </div>
         <div className="form-group">
-          <label className="form-label">Comments</label>
+          <label className="form-label">Written testimony</label>
           <textarea className="form-control" name="txt" autoComplete="off" autofill="off"></textarea>
         </div>
+        <div className="form-group">
+          <label className="form-label">File Upload (optional)</label>
+          <input className="form-control" type="file" id="formFile"/>
+        </div>
+        <hr/>
+
+        <strong>Are you submitting on behalf of an organization? Are you submitting on behalf of others?</strong>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
+          <label class="form-check-label" for="flexRadioDefault1">
+            Yes
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
+          <label class="form-check-label" for="flexRadioDefault1">
+            No
+          </label>
+        </div>
+        <hr/>
+
+        <strong>Is there anything else you would like the Ohio Community Commission to know?</strong>
+        <div className="form-group">
+          <textarea className="form-control">
+          </textarea>
+        </div>
+
         <div className="form-group">
           <button
             className="btn btn-primary"
