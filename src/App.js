61e60bf543a7e2294d6413a4f3d05c4f8c8813da
Nick Doiron <ndoiron@mapmeld.com>
2021-02-08 10:16:44 -0700

    initial bootstrap + react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import AddTutorial from "./components/add-tutorial.component";
+// import Tutorial from "./components/tutorial.component";
+import TutorialsList from "./components/tutorials-list";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/" className="navbar-brand">
+            State Portal
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/start"} className="nav-link">
+                Getting Started
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/comment"} className="nav-link">
+                Share Comments
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
